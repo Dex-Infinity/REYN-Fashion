2a4c36bab963938098c341b79d7030a17f819936
Nana Kofi Agyin <thinker059@gmail.com>
2026-05-20 00:29:51 +0000

    fix: resolve TS schemas import path in config

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
-import { schemaTypes } from "./sanity/schemas";
+import { schemaTypes } from "./sanity/schemas/index";
 
 export default defineConfig({
   name: "default",
