d1c9f3914b19900d41b68b4f02e2bd4f3f88caa3
Nana Kofi Agyin <thinker059@gmail.com>
2026-05-20 00:40:42 +0000

    .

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,6 +1,6 @@
 import { createClient } from "next-sanity";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "placeholder";
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "thzzf5q7";
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-03-11";
 
